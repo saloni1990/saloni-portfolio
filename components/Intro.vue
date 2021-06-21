431001d4a083edf75ce0689a2b0c8d14fce68f43
saloni1990 <salonicmd@gmail.com>
2021-06-21 15:16:50 +0100

    update intro

--- components/Intro.vue
+++ components/Intro.vue
@@ -5,7 +5,7 @@
       <div class=" flex flex-col text-center w-full pt-16 mb-10">
       <h2 class="text-xs text-gray-500 tracking-widest font-medium title-font mb-1 uppercase">About me</h2>
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Saloni Robinson</h1>
-      <p class="lg:w-2/3 mx-auto px-8 leading-relaxed text-base">Hi! I've worked in market research for the past 7 years and recently I have been freelancing as a front-end web developer after learning to code. I have made this website using NuxtJS, a Vue framework. Take a look below to find out more about what I have been about to.</p>
+      <p class="lg:w-2/3 mx-auto px-8 leading-relaxed text-base">Hi! I've worked in market research for the past 7 years and recently I have been freelancing as a front-end web developer after learning to code. I have made this website using NuxtJS, a Vue framework. Take a look below to find out more about what I have been up to.</p>
     </div>
      </div>
   </section>
