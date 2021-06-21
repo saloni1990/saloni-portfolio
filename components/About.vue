3d307af5a2e5e255462bc50afcd1fb335008d7f2
saloni1990 <salonicmd@gmail.com>
2021-06-21 14:03:09 +0100

    update logic to hide education when on about page

--- components/About.vue
+++ components/About.vue
@@ -2,8 +2,8 @@
 <div class="max-w-6xl mx-auto">
   <section class="text-gray-600 body-font">
       <div class="flex justify-center">
-        <button v-on:click="toggleWork" class="inline-flex text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg">Work</button>
-        <button  v-on:click="toggleEducation" class="ml-4 inline-flex text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Education</button>
+        <button v-on:click="toggleWork" class="inline-flex text-white bg-gray-500   focus:text-white hover:bg-gray-400 focus:bg-gray-800 hover:text-white transition duration-150 ease-in-outborder-0 py-2 px-6 focus:outline-none rounded text-lg">Work</button>
+        <button  v-on:click="toggleEducation" class="ml-4 inline-flex text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-gray-400 focus:bg-gray-800 rounded text-lg">Education</button>
       </div>
 </section>
   <section  v-if="showWork" id="work" class="text-gray-600 body-font overflow-hidden">
@@ -11,7 +11,7 @@
   <div class="container px-5 py-10 mx-auto">
     <div class="-my-8 divide-y-2 divide-gray-100">
       <div class="py-8 flex flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col text-left md:text-center">
           <span class="font-semibold title-font text-gray-700 uppercase">Date</span>
           <span class="mt-1 text-gray-500 text-sm">2017-2018</span>
         </div>
@@ -21,13 +21,13 @@
         </div>
       </div>
       <div class="py-8 flex flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col text-left md:text-center">
           <span class="font-semibold title-font text-gray-700 uppercase">Date</span>
           <span class="mt-1 text-gray-500 text-sm">2016 - Current</span>
         </div>
         <div class="md:flex-grow text-left">
           <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Research Analyst - Altro</h2>
-          <p class="leading-relaxed">I work as a research executive help drive the overall strategy of the business and grow our expertise in European and emerging markets. 
+          <p class="leading-relaxed">I work as a research executive to help drive the overall strategy of the business and grow our expertise in European and emerging markets. 
 My research skills also help drive the customer journey experience across the website so that we are always tuned into what the customer requires. 
 My responsibilities also include updating and adding content to the company's website in line with the digital communication plan. I also have to create new pages on the website and upload different types of media content which means that I have to navigate my way around our content management system on a daily basis. 
 I have excellent relationships with the wider business, especially with commercial stakeholders so that they are fully aware of the projects I am working on. I liaise with our web developer on upcoming projects so that new features are displayed on the website.
@@ -35,7 +35,7 @@ SEO is always front of mind to ensure that are analytics are strong. This is don
         </div>
       </div>
       <div class="py-8 flex flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col text-left md:text-center">
           <span class="font-semibold title-font text-gray-700 uppercase">Date</span>
           <span class="text-sm text-gray-500">12 Jun 2019</span>
         </div>
@@ -60,7 +60,7 @@ informed the client how to target consumers to result in brand engagement.</p>
   <div class="container px-5 py-10 mx-auto">
     <div class="-my-8 divide-y-2 divide-gray-100">
       <div class="py-8 flex flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col text-left md:text-center">
           <span class="font-semibold title-font text-gray-700 uppercase">Date</span>
           <span class="mt-1 text-gray-500 text-sm">2018 - Current </span>
         </div>
@@ -71,7 +71,7 @@ informed the client how to target consumers to result in brand engagement.</p>
         </div>
       </div>
       <div class="py-8 flex flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col text-left md:text-center">
           <span class="font-semibold title-font text-gray-700 uppercase">Date</span>
           <span class="mt-1 text-gray-500 text-sm">2016</span>
         </div>
@@ -80,7 +80,7 @@ informed the client how to target consumers to result in brand engagement.</p>
         </div>
       </div>
       <div class="py-8 flex flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col text-left md:text-center">
           <span class="font-semibold title-font text-gray-700 uppercase">Date</span>
           <span class="text-sm text-gray-500">2015</span>
         </div>
@@ -90,7 +90,7 @@ informed the client how to target consumers to result in brand engagement.</p>
         </div>
       </div>
        <div class="py-8 flex flex-wrap md:flex-nowrap">
-        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+        <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col text-left md:text-center">
           <span class="font-semibold title-font text-gray-700 uppercase">Date</span>
           <span class="text-sm text-gray-500">Sep 2009 – Jun 2012</span>
         </div>
@@ -113,7 +113,7 @@ export default {
   data: function () {
     return {
       showWork: true,
-      showEducation: true
+      showEducation: false
     }
   },
   methods: {
