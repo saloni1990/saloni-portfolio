352bcceea474124bbac9f60be81bd95bf1270788
saloni1990 <salonicmd@gmail.com>
2021-06-21 14:03:15 +0100

    update

--- components/Header.vue
+++ components/Header.vue
@@ -2,8 +2,8 @@
     <div class="max-w-full  bg-gray-200">
         <nav class="max-w-6xl mx-auto sm:py-4">
             <div class="mx-auto px-2 sm:px-6 lg:px-8">
-                <div class="relative w-full flex items-center justify-between py-8 sm:py-0">
-                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <div class="relative w-full flex items-center justify-between py-6 sm:py-0">
+                <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
                     <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-600 hover:text-white hover:bg-gray-600 focus:outline-none focus:bg-gray-600 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
                     <!-- Icon when menu is closed. -->
@@ -19,9 +19,9 @@
                     </button>
                 </div>
                 <header class="hidden sm:block text-gray-600 body-font">
-                <div class=" mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
-                    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 pr-8 ">
-                    <span class="ml-3 text-xl">Sal Learns to Code</span>
+                <div class=" mx-auto flex flex-wrapflex-col md:flex-row items-center">
+                    <a href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 pr-8 ">
+                    <span class="text-xl">Sal Learns to Code</span>
                     </a>
                     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <a href="/" class="mr-5 hover:text-gray-900">Home</a>
@@ -38,9 +38,14 @@
                 Menu open: "block", Menu closed: "hidden"
             -->
             <div v-on:click="toggleMenu" v-bind:class="{block: !isOpen, hidden: isOpen}"  class=" sm:hidden">
-                <div class="px-2 pt-2 pb-3">
-                <a href="/" class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-400 focus:outline-none focus:text-white focus:bg-gray-600 transition duration-150 ease-in-out">Home</a>
-                <a href="/about" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-600 focus:outline-none focus:text-white focus:bg-gray-600 transition duration-150 ease-in-out">About</a>
+                <div class="px-2 pt-2 pb-3 flex justify-center flex-col mx-auto w-full items-center">
+                <a href="/" class="flex title-font font-medium items-center justify-center text-center text-gray-900 mb-4 md:mb-0 sm:pr-8 ">
+                    <span class="text-xl">Sal Learns to Code</span>
+                </a>
+                <div class="w-full flex flex-row items-center justify-center">
+                    <a href="/" class="w-1/4 block px-3 py-2 mx-2 rounded-md text-base font-medium text-gray-600  focus:outline-none focus:text-white hover:bg-gray-600 focus:bg-gray-600 hover:text-white transition duration-150 ease-in-out text-center">Home</a>
+                <a href="/about" class=" w-1/4 block px-3 py-2 mx-2 rounded-md text-base font-medium text-gray-600 hover:text-white hover:bg-gray-600 focus:outline-none focus:text-white focus:bg-gray-600 transition duration-150 ease-in-out text-center">About</a>
+                </div>
                 </div>
             </div>
         </nav>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'saloni-portfolio',
+    title: 'Saloni Robinson',
     htmlAttrs: {
       lang: 'en'
     },
