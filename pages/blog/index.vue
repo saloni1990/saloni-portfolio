607d90fa80269fb47b1b654a7f9bf07e430dedc7
saloni1990 <salonicmd@gmail.com>
2021-07-21 20:42:59 +0100

    change branch name to sync blog

---
+++ pages/blog/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="">
+    <div class="w-full">
+      <Header />
+      <Blog />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import Blog from '~/components/Blog.vue'
+import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    Blog,
+    Footer
+  },
+  computed: {
+  blog() {
+    return this.$store.state.blog
+  }
+  }
+  
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
